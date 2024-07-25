721f40d042e2f893fae4c8df0b13ade06dd3520f
Iva <iva_dkostadinova@abv.bg>
2024-07-25 16:20:35 +0300

    Create Movie Card

---
+++ client/src/components/MovieList/MovieCard.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+export const MovieCard = ({
+    _id,
+    title,
+    director,
+    year,
+    genre,
+    img,
+}) => {
+    return (
+        <li>
+            <h3>{title}</h3>
+            <img src={img} alt= {title} />
+            <h5>{director}, <span>{year}</span></h5>
+            <p>{genre}</p>
+            <button><Link to={`/movies/${_id}`} >Details</Link></button>
+        </li>
+    )
+}
