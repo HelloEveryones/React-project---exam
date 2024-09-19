a7339ad533817244a6d5ac5083e22468ce23b725
Iva <iva_dkostadinova@abv.bg>
2024-09-19 13:27:56 +0300

    .

--- client/src/services/wheatherService.js
+++ client/src/services/wheatherService.js
@@ -1,9 +1,9 @@
-const apiKey = "2eb3e03a21484e1a9e5194321240708";
 
+const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
 
 export const getWheather = async () => {
     const data = await getTown();
-    const baseUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${data.city}&aqi=no`;
+    const baseUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${data.city}&aqi=no`;  // Променете на HTTPS
     const response = await fetch(baseUrl);
     if (response.ok) {
         return await response.json();
@@ -12,11 +12,10 @@ export const getWheather = async () => {
 }
 
 const getTown = async () => {
-    const response = await fetch('https://ipapi.co/json/');
+    const response = await fetch('https://ipapi.co/json/');  
     if (response.ok) {
         const data = await response.json();
         return data;
     }
     throw response;
-
 }
