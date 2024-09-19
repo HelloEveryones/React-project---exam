82ec1dfbefb6fc3f6cc37b6ac07c20fa0d2c7191
Iva <iva_dkostadinova@abv.bg>
2024-09-19 14:09:37 +0300

    update api weather key for the deploy

--- client/src/services/wheatherService.js
+++ client/src/services/wheatherService.js
@@ -1,9 +1,10 @@
-const apiKey = "2eb3e03a21484e1a9e5194321240708";
+const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+
 
 
 export const getWheather = async () => {
     const data = await getTown();
-    const baseUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${data.city}&aqi=no`;
+    const baseUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${data.city}&aqi=no`;
     const response = await fetch(baseUrl);
     if (response.ok) {
         return await response.json();
